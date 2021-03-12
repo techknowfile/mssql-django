7bac364fee2c28ba5a0edd53f8bcab75194b4d8f
Andrew Dudley <a0d0358@homeoffice.wal-mart.com>
2021-03-12 13:02:09 -0700

    change temp table type for uuid

--- mssql/functions.py
+++ mssql/functions.py
@@ -98,7 +98,7 @@ def split_parameter_list_as_sql(self, compiler, connection):
 
     with connection.cursor() as cursor:
         cursor.execute("IF OBJECT_ID('tempdb.dbo.#Temp_params', 'U') IS NOT NULL DROP TABLE #Temp_params; ")
-        cursor.execute("CREATE TABLE #Temp_params (params int)")
+        cursor.execute("CREATE TABLE #Temp_params (params char(32))")
         for offset in range(0, len(rhs_params), 1000):
             sqls_params = rhs_params[offset: offset + 1000]
             sqls_params = ", ".join("({})".format(item) for item in sqls_params)
